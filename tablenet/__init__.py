07c68857c6631b20310cfc65f268c6d0e0f2054a
Sai Krishna <saikrishna.gonuguntla@gmail.com>
2023-12-15 03:18:44 -0600

    Fixed some bugs

--- tablenet/__init__.py
+++ tablenet/__init__.py
@@ -1,6 +1,6 @@
 """Utilities for TableNet."""
 
-from .marmot import AuditImageDataModule
+from .AuditData import AuditImageDataModule
 from .tablenet import TableNetModule
 
 __all__ = ['AuditImageDataModule', 'TableNetModule']
